8af202a04ab171b5de84e4d43322bf7db2434af5
relibrana <relibrana@outlook.com>
2022-11-20 17:13:53 -0500

    socialmedia

--- landing-page/src/components/Footer.jsx
+++ landing-page/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from "../images/juan_mostacho.png"
+import facebook from "../images/Facebook_Logo.png"
+import instagram from "../images/Instagram_icon.png"
 
 function Footer() {
   return (
@@ -9,9 +11,15 @@ function Footer() {
             <a href="#main" className='logoo'>
                 <img src={logo} alt="" />
             </a>
+            <a href="https://www.facebook.com/people/Pingunio-Feliz/100087616497296/" className='fb'target="_blank" rel="noopener noreferrer">
+                <img src={facebook} alt="" />
+            </a>
+            <a href="https://www.instagram.com/penguin_os_official/" className='insta'target="_blank" rel="noopener noreferrer">
+                <img src={instagram} alt="" />
+            </a>
         </div>
         <div className='footer-right'>
-            <a href="#" className='button header_cta'>Crear Cuenta</a>
+            <a href="#" className='button_header_cta'>Crear Cuenta</a>
             <h6>© J.U.A.N by PenguinOS All Rights Reserved</h6>
         </div>
         </ul>
